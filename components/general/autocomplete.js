da9d3377f50740148814675ab164ec99a2f8734f
Matt Mawson <mawsonm@umich.edu>
2023-02-10 22:11:17 -0500

    fixed autocomplete issue

--- components/general/autocomplete.js
+++ components/general/autocomplete.js
@@ -50,7 +50,11 @@ const Autocomplete = (props) => {
           className={`w-[50%] min-w-[325px] rounded bg-slate-200 py-2 px-4 focus:bg-slate-100 focus:outline-2 outline-red-200`}
         />
         {
-          <ul className="max-h-[150px] w-[50%] min-w-[325px] overflow-y-scroll flex flex-col bg-slate-100 border-2 border-red-200">
+          <ul
+            className={`${
+              !focused ? "hidden" : ""
+            } max-h-[150px] w-[50%] min-w-[325px] overflow-y-scroll flex flex-col bg-slate-100 border-2 border-red-200`}
+          >
             {recipes
               .filter((recipe) =>
                 recipe.name.toLowerCase().includes(inputValue.toLowerCase())
@@ -60,7 +64,7 @@ const Autocomplete = (props) => {
                   <li
                     key={index}
                     className="px-4 py-2 cursor-pointer hover:bg-slate-200"
-                    onClick={() => handleSelection(item)}
+                    onMouseDown={() => handleSelection(item)}
                   >
                     {item.name}
                   </li>
